7a4c5c23cff5de78832d7177896ab6e9294edd36
injurio <49044584+injurio@users.noreply.github.com>
2021-07-12 11:16:58 +0700

    add store

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import modal from "vue-js-modal";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(modal, { dialog: true, dynamic: true });
 
 new Vue({
   vuetify,
   router,
   render: h => h(App)
+  store
 }).$mount('#app')
